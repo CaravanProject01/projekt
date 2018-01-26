dd321f599725d321d0cf435411b91b25cf6567fe
wardasz <m.wardzinski5991@gmail.com>
2018-01-26 19:55:19 +0100

    plis i minus

--- program/Caravans/Caravans/Zakupy.xaml.cs
+++ program/Caravans/Caravans/Zakupy.xaml.cs
@@ -21,7 +21,7 @@ namespace Caravans
     /// </summary>
     public partial class Zakupy : Window
     {
-        string iljabwag = "1000" ;
+        string iljabwag = "1000";
         string iltreew = "1000";
         string ilmeatt = "1000";
         string ilchlebw = "1000";
@@ -35,6 +35,8 @@ namespace Caravans
         string ibron = "576";
         string csbron = "48.00";
         string br = "0";
+
+
 
         private void sprz_Click(object sender, RoutedEventArgs e)
         {
@@ -164,21 +166,298 @@ namespace Caravans
             ilprzyprawywag.DataContext = this;
             ilskorawag.DataContext = this;
 
+
         }
 
         private void exitZ_Click(object sender, RoutedEventArgs e) => Close();
 
-    
 
-        private void button_Click(object sender, RoutedEventArgs e)
+
+        private void buttonT1_Click(object sender, RoutedEventArgs e)//tkanina sprzedaj
         {
 
         }
 
-        private void button1_Click(object sender, RoutedEventArgs e)
+        private void buttonT2_Click(object sender, RoutedEventArgs e)//tkanina -
+        {
+            int x;
+            x = Convert.ToInt32(tkan.Text);
+            if (x > 0) { x--; }
+            tkan.Text = x.ToString();
+        }
+
+        private void buttonT3_Click(object sender, RoutedEventArgs e)//tkanina kup
         {
 
+        }
+
+        private void buttonT4_Click(object sender, RoutedEventArgs e)//tkanina +
+        {
+            int x;
+            x = Convert.ToInt32(tkan.Text);
+            x++;
+            tkan.Text = x.ToString();
+        }
+
+        private void buttonW1_Click(object sender, RoutedEventArgs e)//wino sprzedaj
+        {
+
+        }
+
+        private void buttonW2_Click(object sender, RoutedEventArgs e)//wino -
+        {
+            int x;
+            x = Convert.ToInt32(wino.Text);
+            if (x > 0) { x--; }
+            wino.Text = x.ToString();
+        }
+
+        private void buttonW3_Click(object sender, RoutedEventArgs e)//wino kup
+        {
+
+        }
+
+        private void buttonW4_Click(object sender, RoutedEventArgs e)//wino +
+        {
+            int x;
+            x = Convert.ToInt32(wino.Text);
+            x++;
+            wino.Text = x.ToString();
+        }
+
+        private void buttonB1_Click(object sender, RoutedEventArgs e)//bron sprzedaj
+        {
+
+        }
+
+        private void buttonB2_Click(object sender, RoutedEventArgs e)//bron -
+        {
+            int x;
+            x = Convert.ToInt32(bron.Text);
+            if (x > 0) { x--; }
+            bron.Text = x.ToString();
+        }
+
+        private void buttonB3_Click(object sender, RoutedEventArgs e)//bron kup
+        {
+
+        }
+
+        private void buttonB4_Click(object sender, RoutedEventArgs e)//bron +
+        {
+            int x;
+            x = Convert.ToInt32(bron.Text);
+            x++;
+            bron.Text = x.ToString();
+        }
+
+        private void buttonC1_Click(object sender, RoutedEventArgs e)//chleb sprzedaj
+        {
+
+        }
+
+        private void buttonC2_Click(object sender, RoutedEventArgs e)//chleb -
+        {
+            int x;
+            x = Convert.ToInt32(hleb.Text);
+            if (x > 0) { x--; }
+            hleb.Text = x.ToString();
+        }
+
+        private void buttonC3_Click(object sender, RoutedEventArgs e)//chleb kup
+        {
+
+        }
+
+        private void buttonC4_Click(object sender, RoutedEventArgs e)//chleb +
+        {
+            int x;
+            x = Convert.ToInt32(hleb.Text);
+            x++;
+            hleb.Text = x.ToString();
+        }
+
+        private void buttonD1_Click(object sender, RoutedEventArgs e)//drewno sprzedaj
+        {
+
+        }
+
+        private void buttonD2_Click(object sender, RoutedEventArgs e)//drewno -
+        {
+            int x;
+            x = Convert.ToInt32(brondrz.Text);
+            if (x > 0) { x--; }
+            brondrz.Text = x.ToString();
+        }
+
+        private void buttonD3_Click(object sender, RoutedEventArgs e)//drewno kup
+        {
+
+        }
+
+        private void buttonD4_Click(object sender, RoutedEventArgs e)//drewno +
+        {
+            int x;
+            x = Convert.ToInt32(brondrz.Text);
+            x++;
+            brondrz.Text = x.ToString();
+        }
+
+        private void buttonJ1_Click(object sender, RoutedEventArgs e)//jabłko sprzedaj
+        {
+
+        }
+
+        private void buttonJ2_Click(object sender, RoutedEventArgs e)//jabłko -
+        {
+            int x;
+            x = Convert.ToInt32(jabl.Text);
+            if (x > 0) { x--; }
+            jabl.Text = x.ToString();
+        }
+
+        private void buttonJ3_Click(object sender, RoutedEventArgs e)//jabłko kup
+        {
+
+        }
+
+        private void buttonJ4_Click(object sender, RoutedEventArgs e)//jabłko +
+        {
+            int x;
+            x = Convert.ToInt32(jabl.Text);
+            x++;
+            jabl.Text = x.ToString();
+        }
+
+        private void buttonM1_Click(object sender, RoutedEventArgs e)//mięso sprzedaj
+        {
+
+        }
+
+        private void buttonM2_Click(object sender, RoutedEventArgs e)//mięso -
+        {
+            int x;
+            x = Convert.ToInt32(mies.Text);
+            if (x > 0) { x--; }
+            mies.Text = x.ToString();
+        }
+
+        private void buttonM3_Click(object sender, RoutedEventArgs e)//mięso kup
+        {
+
+        }
+
+        private void buttonM4_Click(object sender, RoutedEventArgs e)//mięso +
+        {
+            int x;
+            x = Convert.ToInt32(mies.Text);
+            x++;
+            mies.Text = x.ToString();
+        }
+
+        private void buttonP1_Click(object sender, RoutedEventArgs e)//perła sprzedaj
+        {
+
+        }
+
+        private void buttonP2_Click(object sender, RoutedEventArgs e)//perła -
+        {
+            int x;
+            x = Convert.ToInt32(perl.Text);
+            if (x > 0) { x--; }
+            perl.Text = x.ToString();
+        }
+
+        private void buttonP3_Click(object sender, RoutedEventArgs e)//perła kup
+        {
+
+        }
+
+        private void buttonP4_Click(object sender, RoutedEventArgs e)//perła +
+        {
+            int x;
+            x = Convert.ToInt32(perl.Text);
+            x++;
+            perl.Text = x.ToString();
+        }
+
+        private void buttonS1_Click(object sender, RoutedEventArgs e)//skóra sprzedaj
+        {
+
+        }
+
+        private void buttonS2_Click(object sender, RoutedEventArgs e)//skóra -
+        {
+            int x;
+            x = Convert.ToInt32(skur.Text);
+            if (x > 0) { x--; }
+            skur.Text = x.ToString();
+        }
+
+        private void buttonS3_Click(object sender, RoutedEventArgs e)//skóra kup
+        {
+
+        }
+
+        private void buttonS4_Click(object sender, RoutedEventArgs e)//skóra +
+        {
+            int x;
+            x = Convert.ToInt32(skur.Text);
+            x++;
+            skur.Text = x.ToString();
+        }
+
+        private void buttonA1_Click(object sender, RoutedEventArgs e)//alchemia sprzedaj
+        {
+
+        }
+
+        private void buttonA2_Click(object sender, RoutedEventArgs e)//alchemia -
+        {
+            int x;
+            x = Convert.ToInt32(srodki.Text);
+            if (x > 0) { x--; }
+            srodki.Text = x.ToString();
+        }
+
+        private void buttonA3_Click(object sender, RoutedEventArgs e)//alchemia kup
+        {
+
+        }
+
+        private void buttonA4_Click(object sender, RoutedEventArgs e)//alchemia +
+        {
+            int x;
+            x = Convert.ToInt32(srodki.Text);
+            x++;
+            srodki.Text = x.ToString();
+        }
+
+        private void buttonX1_Click(object sender, RoutedEventArgs e)//przyprawy sprzedaj
+        {
+
+        }
+
+        private void buttonX2_Click(object sender, RoutedEventArgs e)//przyprawy -
+        {
+            int x;
+            x = Convert.ToInt32(przyp.Text);
+            if (x > 0) { x--; }
+            przyp.Text = x.ToString();
+        }
+
+        private void buttonX3_Click(object sender, RoutedEventArgs e)//przyprawy kup
+        {
+
+        }
+
+        private void buttonX4_Click(object sender, RoutedEventArgs e)//przyprawy +
+        {
+            int x;
+            x = Convert.ToInt32(przyp.Text);
+            x++;
+            przyp.Text = x.ToString();
         }
     }
-     
+
 }
